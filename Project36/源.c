1134c3ec342c29342ce63822294d208a620fea03
DB <YeQingDinBin@yeah.net>
2020-12-09 15:36:23 +0800

    pta

---
+++ Project36/源.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int n, i, j, max;
+//	int arr[10];
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		max = arr[i];
+//		for (j = i + 1; j < n; j++)
+//		{
+//			if (arr[j] > max)
+//			{
+//				max = arr[j];
+//			}
+//		}
+//		printf("%d ", max);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    char arr[80];
+//    gets(arr);
+//    int i,count=0;
+//    for (i = 0; i < 80; i++)
+//    {
+//        count += 1;
+//        if (arr[i] == '\0')
+//            break;
+//    }
+//    for (i = count-2; i >= 0 ; i--)
+//    {
+//        printf("%c", arr[i]);   
+//    }
+//    return 0;
+//}
