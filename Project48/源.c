dec97207dcbadb10d9509d7f0e11936e589f5874
DB <YeQingDinBin@yeah.net>
2020-12-17 23:07:16 +0800

    pta

---
+++ Project48/源.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string.h>
+//struct book
+//{
+//	char name[100];
+//	float price;
+//};
+//int main()
+//{
+//	struct book b[11];
+//	int n, i=0;
+//	float max = 0, min = 1000;
+//	int max_i, min_i;
+//	scanf("%d\n", &n);
+//	//while (i < n)
+//	//{
+//	//	
+//	//	i++;
+//	//}
+//	for (i = 0; i < n; i++)
+//	{
+//		if (i < n - 1)
+//		{
+//			gets(b[i].name);
+//			//scanf("\n");
+//			scanf("%f\n", &b[i].price);
+//		}
+//		if (i == n - 1)
+//		{
+//			gets(b[i].name);
+//			//scanf("\n");
+//			scanf("%f", &b[i].price);
+//		}
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		if (b[i].price > max)
+//		{
+//			max = b[i].price;
+//			max_i = i;
+//		}
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		if (b[i].price < min)
+//		{
+//			min = b[i].price;
+//			min_i = i;
+//		}
+//	}
+//	printf("%.2f %s\n", b[max_i].price, b[max_i].name);
+//	printf("%.2f %s\n", b[min_i].price, b[min_i].name);
+//	return 0;
+//}
+#include<stdio.h>
+#include<string.h>
+struct friend
+{
+	char name[11];
+	int bron;
+	char number[18];
+};
+int main()
+{
+	struct friend fre[11];
+	int n, i, j, min=0,k=100;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%s %d %s", &fre[i].name, &fre[i].bron, &fre[i].number);
+	}
+	for (i = 0; i < n; i++)
+	{
+		int temp;
+		for (j = 0; j < n - 1 - i; j++)
+		{
+			if (fre[j].bron > fre[j + 1].bron)
+			{
+				temp = fre[j].bron;
+				fre[j].bron = fre[j + 1].bron;
+				fre[j + 1].bron = temp;
+			}
+		}
+	}
+	
+	return 0;
+}
