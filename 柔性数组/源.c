9ed1dcb87708104990ff3191ba6dd41cf9859b5f
DB <YeQingDinBin@yeah.net>
2021-01-03 14:59:24 +0800

    柔性数组.exe

---
+++ 柔性数组/源.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//柔性数组
+//struct S
+//{
+//	int n;
+//	int arr[0];
+//};
+//int main()
+//{
+//	struct S* ps = (struct S*)malloc(sizeof(struct S) + 5 * sizeof(int));
+//	ps->n = 100;
+//	int i=0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	struct S* ptr = (struct S*)realloc(ps, 44);//此处数字为字节数
+//	if (ptr != NULL)
+//	{
+//		ps = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+
+
+//不使用柔性数组方式
+struct S
+{
+	int n;
+	int *arr;
+};
+int main()
+{
+	struct S*ps = (struct S*)malloc(sizeof(struct S));
+	ps->arr = malloc(5 * sizeof(int));
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		ps->arr[i] = i;
+	}
+	int* ptr = realloc(ps->arr, 10 * sizeof(int));
+	if (ptr != NULL)
+	{
+		ps->arr = ptr;
+	}
+	for (i = 5; i < 10; i++)
+	{
+		ps->arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", ps->arr[i]);
+	}
+	free(ps->arr);
+	ps->arr = NULL;
+	free(ps);
+	ps = NULL;
+	return 0;
+}
